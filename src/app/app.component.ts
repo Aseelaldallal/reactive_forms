147d829c17e3bdf186e43ee77e19f807e994d263
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-09-20 20:55:15 -0400

    nested form group

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,20 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
 
   genders = ['male', 'female'];
-  signupForm : FormGroup;
+  signupForm: FormGroup;
 
 
   // Initialize form before rendering template
   ngOnInit() {
     this.signupForm = new FormGroup({
       // controls
-      'username': new FormControl(null, Validators.required), 
-      // Validators.required - just a reference: angular will execute validators.required whenever it detects that the input of the form changed  
-      // quotations so during minification this property code is kept
-      'email' : new FormControl(null, [Validators.required, Validators.email]), // pass an array of validators
-      'gender' : new FormControl('male') // default value is male
+      'userData': new FormGroup({
+        'username': new FormControl(null, Validators.required),
+        // Validators.required - just a reference: angular will execute validators.required whenever it detects that the input of the form changed  
+        // quotations so during minification this property code is kept
+        'email': new FormControl(null, [Validators.required, Validators.email]), // pass an array of validators
+      }),
+      'gender': new FormControl('male') // default value is male
     });
   }
 
