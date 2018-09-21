8a0839c860f5ee36c64de01eefac712b3fcfd7a2
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-09-20 21:29:04 -0400

    create custom validators -- nice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent implements OnInit {
 
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ["Aseel", "Helo"];
 
 
   // Initialize form before rendering template
@@ -17,7 +18,7 @@ export class AppComponent implements OnInit {
     this.signupForm = new FormGroup({
       // controls
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         // Validators.required - just a reference: angular will execute validators.required whenever it detects that the input of the form changed  
         // quotations so during minification this property code is kept
         'email': new FormControl(null, [Validators.required, Validators.email]), // pass an array of validators
@@ -36,5 +37,15 @@ export class AppComponent implements OnInit {
     console.log(this.signupForm);
   }
 
+  // { nameIsForbidden: true}
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    // anguolar is calling this, so 'this' is not what we expect, must bind
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {
+        'nameIsForbidden': true
+      }
+    }
+    return null; // IMPORTANT: if validation is successful, you MUST return nothing (omit return) or null
+  }
 
 }
