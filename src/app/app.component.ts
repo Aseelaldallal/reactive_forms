486bacad511d9cc47a93f3e99d61ba1e2128119c
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-09-20 20:34:55 -0400

    setup form programatically - formGroup and FormControl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   genders = ['male', 'female'];
   signupForm : FormGroup;
+
+
+  // Initialize form before rendering template
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      // controls
+      'username': new FormControl(null), // quotations so during minification this property code is kept
+      'email' : new FormControl(null),
+      'gender' : new FormControl('male') // default value is male
+    });
+   
+  }
 }
